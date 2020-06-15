b64ea7623e7ebcb63888204b122f6c386fd48a63
luisabellan <luis.abellan.dev@gmail.com>
2020-06-16 01:01:37 +0200

    save

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -37,14 +37,16 @@ def cityreader(cities=None):
     with open('cities.csv', newline='') as csvfile:
         citiesreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in citiesreader:
+            if row[0].split()[0] == "city":
+                continue
 
             name = ''.join(row[0])
             lat = ''.join(row[3])
             lon = ''.join(row[4])
             try:
                 cities.append(City(name, lat, lon))
-            except AttributeError:
-                print()
+            except AttributeError as e:
+                print(e)
 
         return cities
 
