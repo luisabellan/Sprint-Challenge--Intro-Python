7a7df1d18175a79e2a658d9feaa780a296eac449
luisabellan <luis.abellan.dev@gmail.com>
2020-06-16 02:52:35 +0200

    done

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -92,10 +92,10 @@ print("\n\n")
 
 # Get latitude and longitude values from the user
 
-#coord1 = input("Enter lat1, lon1: \n")
-#coord2 = input("Enter lat2, lon2: \n")
-coord1 = '45,-100'
-coord2 = '32,-120'
+coord1 = input("Enter lat1, lon1: \n")
+coord2 = input("Enter lat2, lon2: \n")
+#coord1 = '45,-100'
+#coord2 = '32,-120'
 lat1 = coord1.split(',')[0]
 lon1 = coord1.split(',')[1]
 lat2 = coord2.split(',')[0]
